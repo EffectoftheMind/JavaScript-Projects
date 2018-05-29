545307ff5965276e896902ad2202e08e4c88a34c
EffectoftheMind <39631708+EffectoftheMind@users.noreply.github.com>
2018-05-29 13:10:42 -0500

    Update spaceshooter.js

--- spaceshooter.js
+++ spaceshooter.js
@@ -1,3 +1,17 @@
+/* HTML:
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <title>repl.it</title>
+    <link href="index.css" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+    <script src="index.js"></script>
+  </body>
+</html> */
+
 window.onload = function(){
   canvas = document.createElement("canvas");
   //define width and height
